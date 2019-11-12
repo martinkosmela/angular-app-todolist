34f245c13b08de4330678218410743614789c078
Marcin Kosmela <marcin.kosmela@immediate.co.uk>
2019-11-12 10:58:02 +0000

    todo components

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service'; 
+
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  
+  todos:Todo[];
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+    // get data from service - hardcoded
+    // this.todos = this.todoService.getTodos();
+
+    // get data from service API
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+
+    // this.todos = [
+    //   {
+    //     id: 1,
+    //     title: 'Todo One',
+    //     completed: false
+    //   },
+    //   {
+    //     id: 2,
+    //     title: 'Todo Two',
+    //     completed: true
+    //   },
+    //   {
+    //     id: 3,
+    //     title: 'Todo Three',
+    //     completed: false
+    //   }
+    // ]
+  }
+
+  deleteTodo(todo:Todo) {
+    // remove from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    // remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+}
